1e40d44459b1e143352eb88e8b14fa96fb61546a
Tina <migujeong@gmail.com>
2023-05-09 01:31:45 +0900

    [refact] 코드 리뷰 반영

--- src/main/java/world/tina/bulletinboard/domain/Category.java
+++ src/main/java/world/tina/bulletinboard/domain/Category.java
@@ -1,6 +1,7 @@
 package world.tina.bulletinboard.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import lombok.Getter;
@@ -14,6 +15,9 @@ public class Category {
 	private final List<Long> childrenIds;
 
 	public Category(Long id, String name, Long parentId) {
+		if (id == null || name == null) {
+			throw new IllegalArgumentException("id 와 name은 null 일 수 없습니다.");
+		}
 		this.id = id;
 		this.name = name;
 		this.parentId = parentId;
@@ -21,7 +25,13 @@ public class Category {
 	}
 
 	public void addChildId(Long childId) {
+		if (childId == null) {
+			throw new IllegalArgumentException("childId는 null 일 수 없습니다.");
+		}
 		this.childrenIds.add(childId);
 	}
 
+	public List<Long> getChildrenIds() {
+		return Collections.unmodifiableList(childrenIds);
+	}
 }
